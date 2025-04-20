bd12c58e195ab1ac30d8c86f93b86c6299ff6314
Arnavk271 <cs24b2045@iiitdm.ac.in>
2025-04-20 13:21:25 +0530

    Update subscriber2.py

--- Problem_2_Task0/subscriber2.py
+++ Problem_2_Task0/subscriber2.py
@@ -6,16 +6,16 @@ from std_msgs.msg import Int32
 
 def callback(msg):
 
-    result = msg.data + 5
+    result = msg.data + 5 # we use this to add 5 as mention in the question
     
-    rospy.loginfo(f"Received: {msg.data}, After *2,*10 we just add 5 to get: {result}")
+    rospy.loginfo(f"Received: {msg.data}, After *2,*10 we just add 5 to get: {result}") #we take multiple of 2 and then multiply by 10 and at the end add 5
     rospy.seleep(1)
     
     
 
 def subscriber2():
 
-    rospy.init_node('subscriber2_node', anonymous=True)
+    rospy.init_node('subscriber2_node', anonymous=True)#subscriber2 calls subscriber1 and then it functions
     
     rospy.Subscriber('multiplied_by_10', Int32, callback)
     
@@ -25,7 +25,7 @@ def subscriber2():
 
 if __name__ == '__main__':
     try:
-        subscriber2()
+        subscriber2() #calling subscriber2
     except rospy.ROSInterruptException:
         pass
 
