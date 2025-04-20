586adcc13f9dddd45d944f0a0ebc7feab5350928
Arnavk271 <cs24b2045@iiitdm.ac.in>
2025-04-20 13:26:58 +0530

    Update publisher_cv.py

--- Problem_2_Task1/publisher_cv.py
+++ Problem_2_Task1/publisher_cv.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 import rospy
-import depthai as dai
+import depthai as dai#depth ai is imported
 
 
-import cv2
+import cv2 #imported cv2 cam
 
 
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge#bridge is used to get the image from opencv to ros
 
 def main():
 
     rospy.init_node("edge_publisher_node")
-    pub = rospy.Publisher("edge_image", Image, queue_size=10)
+    pub = rospy.Publisher("edge_image", Image, queue_size=10)#publisher is used 
     
     
-    bridge = CvBridge()
+    bridge = CvBridge()#the bridge thing mentioned earlier
 
-    pipeline = dai.Pipeline()
+    pipeline = dai.Pipeline()#pipeline is used 
     cam = pipeline.createColorCamera()
     cam.setPreviewSize(640, 480)
     cam.setBoardSocket(dai.CameraBoardSocket.RGB)
