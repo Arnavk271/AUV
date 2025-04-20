37ccf201b0d3b1c23833fea44cb1637f4c65359e
Arnavk271 <cs24b2045@iiitdm.ac.in>
2025-04-20 13:17:59 +0530

    Update publisher1.py

--- Problem_2_Task0/publisher1.py
+++ Problem_2_Task0/publisher1.py
@@ -5,16 +5,16 @@ from std_msgs.msg import Int32
 
 def publisher():
 
-    x = 2 
+    x = 2 #used because the question says multiple of 2
     
     rospy.init_node('publisher_node', anonymous=True)
-    pub = rospy.Publisher('multiple_of_2', Int32, queue_size=10)
+    pub = rospy.Publisher('multiple_of_2', Int32, queue_size=10) #publisher
     rate = rospy.Rate(1) 
  
     while not rospy.is_shutdown():
         pub.publish(x)
         rospy.loginfo(f"Output: {x}")
-        x = x + 2 
+        x = x + 2 #used for table of 2, 2,4,6,......
         rate.sleep()
 
 if __name__ == '__main__':
