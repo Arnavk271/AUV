2d1b638f99487ff888c6fb31313fb2b1a70b9171
Arnavk271 <cs24b2045@iiitdm.ac.in>
2025-04-20 13:24:03 +0530

    Update subscriber3.py

--- Problem_3_Task0/subscriber3.py
+++ Problem_3_Task0/subscriber3.py
@@ -7,14 +7,14 @@ from bot.msg import bot_position
 
 def callback(msg):
 
-    rospy.loginfo(f"Bot Position ->> (x, y, direction) = ({msg.x}, {msg.y}, {msg.direction})")
+    rospy.loginfo(f"Bot Position ->> (x, y, direction) = ({msg.x}, {msg.y}, {msg.direction})") #calling the function to give the result
 
 
 
 def listener():
     rospy.init_node('bot_listener_node', anonymous=True)
     
-    rospy.Subscriber('bot_position', bot_position, callback)
+    rospy.Subscriber('bot_position', bot_position, callback)#call of subscriber
     rospy.spin()
     
     
@@ -22,7 +22,7 @@ def listener():
 
 if __name__ == '__main__':
     try:
-        listener()
+        listener()#call of listener
     except rospy.ROSInterruptException:
         pass
 
